1c1129da4a28e4ded2d09ff832cf82281183d4c3
irin <puhkaya@selectel.ru>
2021-03-24 21:12:49 +0300

    add elements module

---
+++ source/js/elements.js
@@ -0,0 +1,9 @@
+const advertForm = document.forms['ad-form'];
+const filterForm = document.forms['filters'];
+const addressInput = document.querySelector('#address');
+
+export {
+  advertForm,
+  filterForm,
+  addressInput
+}
